ebc318625df87f6e937c829af441d8697de0656b
name <koncevay97@mail.ru>
2024-03-17 15:52:36 +0300

    Ht lesson 5

--- lesson_4/main.py
+++ lesson_4/main.py
@@ -3,7 +3,6 @@
 # Добавить к каждому списку по одному элементу в конец и в начало.
 # Расширить первый список вторым.
 # Все изменения выводить на экран.
-
 list_1 = [1, 2, 3, 4]
 list_2 = ['spring', 'summer', 'autumn']
 print(list_1, list_2)
@@ -28,10 +27,33 @@ print(dict_first)
 # cложить в словарь такого типа {“is”: 4, “and”: None, “or”: 100}
 # Замена словa: Замените все вхождения слова "is" в Дзене Питона на "is nothing
 
-import this
+dzen_of_python = """ Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
 
+print(len(dzen_of_python))
 
+keywords = ["is", "and", "or"]
+keyword_counts = {keyword: dzen_of_python.count(keyword) for keyword in keywords}
+print(keyword_counts)
 
+print(dzen_of_python.replace('is','is nothing'))
 
 
 
