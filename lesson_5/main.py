08d5ea4e16e2ca366a2f064d09d3868ef36121e2
name <koncevay97@mail.ru>
2024-03-17 16:44:43 +0300

    Ht lesson 5

--- lesson_5/main.py
+++ lesson_5/main.py
@@ -25,19 +25,16 @@ if len(string) < 9:
 
 # Получить на ввод количество рублей и копеек и вывести в правильной форме: например, 3 рубля, 1 рубль 25 копеек, 3 копейки.
 # До 10р *
-# До 100р **
-# До 1000р ***
-
 rubles = int(input("Введите количество рублей: "))
 penny = int(input("Введите количество копеек: "))
 if rubles < 10:
     if rubles == 1:
-        rubles = "1 копейка"
+        rubles = "1 рубль"
     else:
-        rubles_str = str(rubles) + "рубля"
+        rubles_str = str(rubles) + "рублей"
 
     if penny == 1:
-        penny = "1 рубль"
+        penny = "1 копейка"
     else:
         penny_str = str(penny) + "копейки"
 
@@ -45,3 +42,25 @@ if rubles < 10:
         print(rubles_str + ", " + penny_str)
 else:
     print("Ввести количество рублей меньше 10")
+
+# До 100р **
+rubles = int(input("Введите количество рублей: "))
+penny = int(input("Введите количество копеек: "))
+if 10 < rubles < 100:
+        rubles_str = str(rubles) + "рублей"
+        penny_str = str(penny) + "копейки"
+if 10 < rubles < 100 and penny > 0:
+        print(rubles_str + ", " + penny_str)
+else:
+    print("Ввести количество рублей меньше 100, но больше 10")
+
+# До 1000р ***
+rubles = int(input("Введите количество рублей: "))
+penny = int(input("Введите количество копеек: "))
+if 100 < rubles < 1000:
+        rubles_str = str(rubles) + "рублей"
+        penny_str = str(penny) + "копейки"
+if 100 < rubles < 1000 and penny > 0:
+        print(rubles_str + ", " + penny_str)
+else:
+    print("Ввести количество рублей меньше 1000, но больше 100")
